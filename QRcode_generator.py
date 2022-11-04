dbadbfb7452cbfea8898e08c76280e703335921f
unknown <ilemobayojoseph06@gmail.com>
2022-11-04 22:24:56 +0100

    Modified

--- QRcode_generator.py
+++ QRcode_generator.py
@@ -2,10 +2,10 @@ import pyqrcode
 from pyqrcode import QRCode
 
 # String represents the QRcode.
-link = "https://www.makeuseof.com/python-projects-for-beginners/"
+link = "https://www.youtube.com/watch?v=_uefYX5ACZ8"
 
 # Generate QRCode
 code = pyqrcode.create(link)
 
 # Create and save the png file naming "myqr.png"
-code.svg("firstQRcode.svg", scale = 8)
+code.svg("secondQRcode.svg", scale = 8)
